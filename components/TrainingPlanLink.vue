dac55dc9ceacd0190bd6287470c6c383099e02c2
heschwerdt <h.schwerdtner@tu-berlin.de>
2021-10-18 12:58:49 +0200

    Setup firebase

---
+++ components/TrainingPlanLink.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-sheet elevation="5" height="65" shaped width="100%">
+    <div class="d-flex align-center" style="height: 100%">
+      <div class="ml-3 mt-1 mb-1" style="height: 100%">
+        <div
+          class="d-flex flex-column justify-space-between"
+          style="height: 100%"
+        >
+          <div class="text-h6">Trainingsplan: BA</div>
+          <div class="text-caption">
+            by <span>Heschwerdt</span> | Updated: <span>Heschwerdt</span>
+          </div>
+        </div>
+      </div>
+      <v-spacer />
+      <div class="mr-4">
+        <v-progress-circular
+          :value="80"
+          color="blue-grey"
+        ></v-progress-circular>
+      </div>
+      <div class="mr-2">
+        <v-icon color="blue-grey" large>mdi-chevron-right</v-icon>
+      </div>
+    </div>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  props: {
+    trainingPlanId: {
+      type: String,
+      default: "Test",
+    },
+    userName: {
+      type: String,
+      default: "TestUser",
+    },
+    trainingPlanName: {
+      type: String,
+      default: "BA Tests",
+    },
+    percentage: {
+      type: Number,
+      default: 0.8,
+    },
+  },
+};
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,31 @@
-import colors from 'vuetify/es5/util/colors'
+/* eslint-disable no-undef */
+const colors = require("vuetify/es5/util/colors").default;
 
-export default {
+module.exports = {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - trainingsplan',
-    title: 'trainingsplan',
+    titleTemplate: "%s - trainingsplan",
+    title: "trainingsplan",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -36,20 +33,46 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
+  modules: ["@nuxtjs/firebase"],
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API,
+      authDomain: process.env.FIREBASE_AUTHDOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASURMENT_ID,
+    },
+    services: {
+      auth: {
+        persistence: "local", // default
+        initialize: {
+          onAuthStateChangedMutation: "ON_AUTH_STATE_CHANGED_MUTATION",
+          subscribeManually: false,
+        },
+      },
+      firestore: {
+        enablePersistence: {
+          synchronizeTabs: true,
+        },
+      },
+      performance: true,
+      analytics: true,
+      remoteConfig: true,
+    },
+  },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -58,13 +81,14 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
-}
+    extractCss: true,
+  },
+};
